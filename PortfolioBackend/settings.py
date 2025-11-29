e1e19624c6c6e59b60578987a2b611a890bf42d7
Nikhil-gusain <nikstudyc03@gmail.com>
2025-11-29 15:34:16 +0530

    static root added

--- PortfolioBackend/settings.py
+++ PortfolioBackend/settings.py
@@ -141,6 +141,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = "/PortfolioBackend/staticfiles"
+
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')    
